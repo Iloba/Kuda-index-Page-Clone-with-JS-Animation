208a5247b4f7427ae1231cb9d1356cf5a3cdc332
Iloba <scarlettsney@gmail.com>
2021-02-02 15:22:10 +0100

    done

---
+++ index.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <title>Kuda. - Join the bank of the free</title>
+    <meta name="author" content="Kuda Bank">
+    <meta name="description" content="">
+    <meta property="og:site_name" content=".">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+
+    <meta property="og:image" content="">
+    <meta property="og:description" content="">
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<link rel="Icon"  href="img/reallogo.svg">
+	<link rel="stylesheet" href="css/bootstrap.css">
+       <!-- Optional JavaScript -->
+      <script type="text/javascript" src="bootstrap.min.js"></script>
+      <script type="text/javascript" src="js/bootstrap.js"></script>
+       <!--animate Css-->
+      <link rel="stylesheet" type="text/css" href="animate.css">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+			
+	<!--ShareThis BEGIN-->
+    <!--ShareThis END-->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5cd57de93255ff0012e3812e&product=sticky-share-buttons&cms=website' async='async'></script>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header id="top">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light  fixed-top" >
+			<a href="index.php" class="navbar-brand py-0"><img class="reallogo" src="img/reallogo.svg"></a>
+			<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarmenu">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarmenu">
+				<ul class="	navbar-nav	ml-auto" >
+					<li class="nav-item"><a id="blog" href="" class="nav-link py-0 ">Blog</a></li>
+					<li class="nav-item"><a  id="support" href="" class="nav-link py-0">Support</a></li>
+					<li class="nav-item"><a  id="get" href="" class="nav-link py-0">Get Kuda</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header><br> <br><br><br><br><br><br>
+	<div class="container-fluid" data-aos="fade-up" data-aos-duration="1000">
+		<div class="sec1">
+			<div class="">
+				<div class="row">
+				<div class="col-md-6">
+					<h1 class="were">We’re the <br>
+						bank of the free.
+					</h1>
+					<p class="most">
+						Most banks exist to tie you down with charges. We're not most banks. Kuda is designed for your smartphone, free of ridiculous charges and great at helping you budget, spend smartly and save more.
+					</p><br>
+					<h5 class="join">Join us, set yourself free.</h5> <br> <br>
+					<a id="get_button" href="#">Get Kuda</a> <br> <br>
+				</div> <br>
+				<div class="col-md-6"> <br>
+					<div id="second_section">
+						<div>
+							<span id="card_span"><img  id="card" class="img-fluid" src="img/card.png"></span>
+							<span id="phone_span"><img id="phone" class="img-fluid " src="img/phone.png"></span>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec2" data-aos="fade-up" data-aos-duration="2000" >
+		<a href="#free">
+			<div class="smalldiv">
+				<img id="arrow" class=" rounded mx-auto d-block" src="img/adown.svg">
+			</div>
+		</a>
+	</div> <br> <br>
+	<div class="sec3" data-aos="fade-up" data-aos-duration="2000">
+		
+			<div class="">
+				<h4 class="join">You’re always on your phone.
+					So is Kuda.
+				</h4>
+			</div>
+		
+	</div> 
+	<div id="free" class="sec4" data-aos="fade-up" data-aos-duration="2000">
+		<h1 class="your">Your Phone + Our App = <span  class="green"><marquee scrollamount="40">Free Withdrawal, Smarter Spendind, A Simpler Life</marquee></span></h1>
+	</div>
+	<div class="sec5" data-aos="fade-up" data-aos-duration="2000">
+		<div>
+			<h3 class="most">
+				Download the app, sign up in minutes and we’ll  <br>send you a debit card. That’s all you need to make <br> the right money moves. No sweat.
+			</h3>
+		</div>
+	</div>
+	<div class="container-fluid" data-aos="fade-up" data-aos-duration="2000">
+		<div class="sec6">
+			<div class="row">
+				<div class="col-md-6" id="s1">
+					<h4 class="join">Save it, spend it, send it. It’s up to you.</h4> <br>
+					<h1 class="were">
+						Total control. <br>
+						Your money is your money.
+					</h1> <br>
+					<p class="most">
+						Whatever you want to do with your money, you can do it from the app. We’ll make sure it’s done better and hassle-free.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<img class="img-fluid" src="img/control.png">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec7" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4" id="sec11"> 
+					<img class="rounded mx-auto d-block img-fluid" id="mg" src="img/promo1.png"> <br>
+					<h4 class="weres2">Account <br> Maintenenance</h4> <br>
+					<h3 class="weres2">Free</h3>
+				</div> 
+				<div class="col-md-1">&nbsp;</div>
+				<div class="col-md-7">
+					<div class="sec8">
+						<img class="card2" src="img/card.png">
+						<div>
+							<h5 class="weres">Card  Maintenance </h5>
+							<h3 class="weres">Free</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec9" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3" id="sec1"> <br> <br>
+					<h1>0.00</h1><br> <br> <br>
+					<h4>Withdrawal Fee</h4> <br>
+					<h4>Free</h4>  <br> <br>
+				</div>
+				<div class="col-md-1">&nbsp;</div>
+				<div class="col-md-3" id="sec2">
+					<img class="rounded mx-auto d-block img-fluid" src="img/atm.png"> <br>
+					<h4 class="weres">ATM  WITHDRAW AT <br> Partner  Banks</h4> <br>
+					<h3 class="weres">Free</h3>
+				</div>
+				<div class="col-md-1">&nbsp;</div>
+				<div class="col-md-3" id="sec3">
+					<img class="rounded mx-auto d-block img-fluid" src="img/transfer.png"> <br>
+					<h4 class="weress">Transfer to <br> *Other   Banks</h4> 
+					<h3 class="weress">Free</h3>
+				</div>
+				<div class="col-md-1">&nbsp;</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec10" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7" id="pec1">
+					<img class="img-fluid lekki rounded mx-auto d-block" src="img/lekki.png">
+					<h1 class="weress1">Instant Notification</h1>
+					<h3 class="weress1">Free</h3>
+				</div>
+				<div class="col-md-1">&nbsp;</div>
+				<div class="col-md-4" id="pec2">
+					<h1>0.00</h1>
+					<br>
+					<h3 style="text-align: left;">Deposit charges**</h3>  <br>
+					<h3 style="text-align: left;">Free</h3> <br>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec11" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6" id="pad">
+					<h3 class="join">Completely paperless. Sign up anywhere.</h3> <br> <br>
+					<p class="most">
+						No more asking for pens in the branch.
+						Seriously, joining Kuda is easy:
+					</p> <br> <br>
+					<h1 class="were">
+						Download the app on Google <br> Play or the App Store.
+					</h1> <br> <br> <br> <br>
+					<div class="apps">
+						<span><img class="d3" src="img/app.svg"></span>
+						<span><img class="d3" src="img/app3.svg"></span>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<h3 class="step">Step 01</h3>
+				</div>
+				<div class="col-md-4">
+					<img class="img-fluid" src="img/p2.png">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec11" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container">
+			<div class="sec111">
+				<h3 class="text-center sec111">
+				A real bank. <br>
+				With Aso Rock-level security.
+				</h3>
+			</div>
+		</div>
+	</div>
+	<div class="sec12" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<img  class="ins" src="img/insured.png"> <br> <br>
+					<h3 class="f1">Insured</h3> <br>
+					<p class="most1">Rest easy, Kuda is licensed by the Central Bank, and your money is insured.</p>
+				</div>
+				<div class="col-md-4">
+					<img  class="ins" src="img/sec.png"> <br> <br>
+					<h3 class="f1">Lost Card</h3> <br>
+					<p class="most1">Life happens. Sometimes debit cards go missing. If that ever happens you can freeze your missing card on the app, and even unfreeze it if you find it later. Simple.</p>
+				</div>
+				<div class="col-md-4">
+					<img  class="ins" src="img/prev.png"> <br> <br>
+					<h3 class="f1">Prevention</h3> <br>
+					<p class="most1">Kuda’s bank-level security protects your sensitive personal information and prevents unauthorized use.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec13" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6" id="set1">
+					<h1 class="were">
+						Need help? <br>
+						Just reach out.
+					</h1> <br> 
+					<p class="most">
+						We’re always here for you. We might be a bank in an app but we don't leave our customers hanging. Everything about us is on the app, but if you ever need help (or you just want to chat), we’ve got you.
+					</p>
+					<span class="join"><span>Call us on</span> +234 903 220 0350 or email help@kudabank.com</span>
+				</div>
+				<div class="col-md-6" id="set2">
+					<img class="img-fluid" src="img/support1.png">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec14" data-aos="fade-up" data-aos-duration="2000">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<img class="img-fluid" src="img/reasons.png">
+				</div>
+				<div class="col-md-6">
+					<h5 class="join">You'll always be able to do more for yourself with Kuda.</h5> <br>
+					<h1 class="were">Need more reasons to set <br> yourself free?</h1>
+					<p class="most text-center"><img class="smallhand" src="img/hand.png"> Save as much as you want automatically and we'll pay you up to 15% interest yearly, no excuses.</p><br>
+					<p class="most"><img class="smallhand" src="img/hand.png"> Plan and keep track of your spending on the app with smart  budgets.</p><br>
+					<p class="most"><img class="smallhand" src="img/hand.png"> Qualify for quick loans in the near future.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="sec15" data-aos="fade-up" data-aos-duration="2000">
+		<div class="flex">
+			<h2 class="text-center were">Download Kuda and dump bank <br> charges for good.</h2> <br>
+			<div class="apps" id="center">
+				<span>	<img src="img/app.svg"></span>
+				<span><img src="img/app3.svg"></span>
+			</div>
+		</div>
+	</div>
+	<footer data-aos="fade-up" data-aos-duration="2000">
+		<div class="footerbox">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<h3 class="footer_head">About Us</h3> <br>
+						<p class="line">
+							<a class="footer_link" href="#">Blog</a> <br>
+							<a class="footer_link" href="#">Press</a> <br>
+							<a class="footer_link" href="#">Join Our Team</a> <br>
+						</p>
+					</div>
+					<div class="col-md-3">
+						<h3 class="footer_head">Support</h3> <br>
+						<p class="line">
+							<a class="footer_link" href="#">FAQs</a> <br>
+							<a class="footer_link" href="#">Contact Us</a> <br>
+							<a class="footer_link" href="#">Security</a> <br>
+						</p>
+					</div>
+					<div class="col-md-3">
+						<h3 class="footer_head">Transperency</h3> <br>
+						<p class="line">
+							<a class="footer_link" href="#">Fees</a> <br>
+							<a class="footer_link" href="#">Privacy Policy</a> <br>
+							<a class="footer_link" href="#">Cookie Policy</a> <br>
+						</p>
+					</div>
+					<div class="col-md-3">
+						<h3 class="footer_head">Patners</h3> <br>
+						<p class="line">
+							<a class="footer_link" href="#"><img src="img/ac.png"></a> <br>
+							<a class="footer_link" href="#"><img src="img/ndic.png"></a> <br>
+							<a class="footer_link" href="#"><img src="img/visa.png"></a> <br>
+						</p>
+					</div>
+				</div>
+			</div> <br>
+			<div class="container">
+					<div class="apps" >
+						<a href="#"><img id="ic1" src="img/app22.png"></a>
+						<a href="#"><img id="ic1" src="img/app33.png"></a>
+						<a href="#"><img id="ic1" src="img/kuda2.png"></a>
+						<a href="#"><img id="ic1" src="img/kuda1.png"></a>
+					</div>
+			</div> <br> <br>
+			<div class="container">
+				<div class="apps">
+					<a href="#"><img class="icons" src="img/facebook2.svg"></a> &nbsp; &nbsp;
+					<a href="#"><img class="icons" src="img/instagram.svg"></a>&nbsp; &nbsp;
+					<a href="#"><img class="icons" src="img/twitter.svg"></a>&nbsp; &nbsp;
+					<a href="#"><img class="icons" src="img/medium.svg"></a>&nbsp;
+				</div>
+			</div>
+			 <br> <br>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="footer_text">*Kuda Customers get 25 free transfer to other banks every month. Extra transfers to other banks cost N10 each.
+							**Kuda account holders can deposit money in over 10,000 branches across Nigeria for free.
+						</p> <br>
+						<p class="footer_text2">
+							© 2020 Kuda MF Bank (RC796975). All rights reserved. All deposits are insured by the Nigerian Deposit Insurance Corporation (NDIC). Kuda MF Bank is licensed by the Central Bank of Nigeria. “Kuda” and “Kudabank” are trademarks of Kuda Technologies Ltd Lagos: 151 Herbert Macaulay Way, Yaba, Lagos. London: 131 Finsbury Pavement, London, EC2A 1NT, UK
+						</p> <br>
+						<p class="footer_text2">
+							Kuda Current Account is provided through Kuda MF Bank. Kuda card is issued by access bank, pursuant to a license from Visa International & Verve. All text, graphics, audio files, code, downloadable material, and other works on this website are the copyrighted works of Kuda MF Bank. All Rights Reserved. Any unauthorized redistribution or reproduction of any copyrighted materials on this website is strictly prohibited. Other product and company names are trademarks of their respective owners. This website contains simulated images; actual appearance may vary.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	
+<script type="text/javascript">
+	swal({
+  title: "Welcome! to Kuda Bank",
+  text: "Welcome to Kuda Bank",
+  icon: "success",
+  button: "Ok!",
+});
+</script>
+<!--Change Nav Bg on Scroll-->
+	<script>
+		$(window).scroll(function(){
+			$('nav').toggleClass('scrolled', $(this).scrollTop() > 700);
+		}); 
+	</script>
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
+</body>
+</html>
